779a5ac38dcc188ae6a2cd843a9ba6ba861dce27
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-04-21 16:33:52 +0200

    Fix quotes in user-data

--- src/main/resources/user-data.sh
+++ src/main/resources/user-data.sh
@@ -20,7 +20,7 @@ cp /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 apt-get install git -y
 echo "Installing zabbix-agent for server at $MONITORING_IP"
 sudo apt-get install -y zabbix-agent
-sudo sed -i -e 's/ServerActive=127.0.0.1/ServerActive=$MONITORING_IP:10051/g' -e 's/Server=127.0.0.1/Server=$MONITORING_IP/g' -e 's/Hostname=Zabbix server/#Hostname=/g' /etc/zabbix/zabbix_agentd.conf
+sudo sed -i -e "s/ServerActive=127.0.0.1/ServerActive=$MONITORING_IP:10051/g" -e "s/Server=127.0.0.1/Server=$MONITORING_IP/g" -e "s/Hostname=Zabbix server/#Hostname=/g" /etc/zabbix/zabbix_agentd.conf
 sudo service zabbix-agent restart
 sudo rm zabbix-release_2.2-1+precise_all.deb
 echo "finished installing zabbix-agent!"
