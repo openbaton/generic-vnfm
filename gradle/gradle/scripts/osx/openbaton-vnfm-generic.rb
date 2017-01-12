523ba62bcf073ad7f827d64f40ffa09280935ad4
Marcello Monachesi <marco.mantis@gmail.com>
2017-01-12 17:06:43 +0100

    Fix: fix formula typo and script improving

--- gradle/gradle/scripts/osx/openbaton-vnfm-generic.rb
+++ gradle/gradle/scripts/osx/openbaton-vnfm-generic.rb
@@ -1,8 +1,8 @@
 class OpenbatonVnfmGeneric < Formula
   desc "Formula of Open Baton VNFM Generic"
   homepage "http://www.openbaton.org"
-  url "https://codeload.github.com/openbaton/generic-vnfm/legacy.tar.gz/develop"
-  version "3.1.1-SNAPSHOT"
+  url "https://codeload.github.com/openbaton/generic-vnfm/legacy.tar.gz/3.1.0"
+  version "3.1.0"
   # sha256 "7a6fde8dd84f09b935b75d85cb1e58327007f677ebc05f8b31d8e46132725fc5"
 
   depends_on :java => "1.7+"
@@ -15,12 +15,12 @@ class OpenbatonVnfmGeneric < Formula
     # Change external path with /usr/local/etc instead /etc (due to brew internal directories)
     inreplace "build/install/generic-vnfm/bin/openbaton-vnfm-generic", "CONFIG_FILE=/config/file/path", "CONFIG_FILE=#{etc}/openbaton/vnfm/generic/application.properties"
     # Change application path
-    inreplace "build/install/generic-vnfm/bin/openbaton-vnfm-generic", /APP_HOME="`pwd -P`"/, %(APP_HOME="#{libexec}")
+    inreplace "build/install/generic-vnfm/bin/openbaton-vnfm-generic", /APP_HOME="`pwd -P`"/, %Q(APP_HOME="#{libexec}")
 
     # Copy the openbaton.properties in the right location
     openbaton_vnfm_generic_properties_path = etc+"openbaton/vnfm/generic"
     openbaton_vnfm_generic_properties_path.mkpath
-    openbaton_vnfm_generic_properties_path.install "src/main/resources/application.properties" , "src/main/resources/user-data.sh" , "src/main/resources/conf.properties"
+    openbaton_vnfm_generic_properties_path.install "src/main/resources/application.properties", "src/main/resources/user-data.sh", "src/main/resources/conf.properties"
 
     # Remove Windows file
     rm_f Dir["build/install/generic-vnfm/bin/*.bat"]
